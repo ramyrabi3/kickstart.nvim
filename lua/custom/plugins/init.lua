79c2af76f09ca7d4dc98f22ed0911ae39fbbde0a
Ramy Rabie <1336373+ramyrabi3@users.noreply.github.com>
2023-07-29 22:03:30 +0200

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,25 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {
+      sort_by = "case_sensitive",
+      view = {
+        width = 30,
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filter = {
+        dotfiles = true,
+      },
+    }
+  end,
+}
