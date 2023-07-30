cbdcdf057a8f14897c960a77f8b30351e8a68e5e
Ramy Rabie <1336373+ramyrabi3@users.noreply.github.com>
2023-07-30 11:01:49 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -40,6 +40,8 @@ P.S. You can delete this when you're done too. It's your config now :)
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ','
 vim.g.maplocalleader = ','
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
@@ -257,6 +259,7 @@ vim.o.nowrap = true
 vim.o.expandtab = true
 vim.o.showtabline = true
 vim.o.autowriteall = true
+vim.opt.termguicolors = true
 
 
 -- [[ Basic Keymaps ]]
